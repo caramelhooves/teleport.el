caramelhooves/teleport.el

commit 15e6148d0f8cb70de6adbe4099689735bcf963e9

feat: kill the running process.

teleport.el
@@ -253,16 +253,17 @@ terminates, call COMPLETION-NOTIFICATION."
 
 (defun teleport--tsh-cmd-async-cached (output-json-symbol process-symbol completion-notification cmd)
   "Start CMD asynchronously if PROCESS-SYMBOL process is not running.
-Store the result in OUTPUT-JSON-SYMBOL and call
-COMPLETION-NOTIFICATION when done."
+If the process is running, kill it and start again. Store the result in
+OUTPUT-JSON-SYMBOL and call COMPLETION-NOTIFICATION when done."
 
   (unless completion-notification
     (setq completion-notification
           (lambda () (message "Teleport cmd done: %s" cmd))))
 
   (let ((old-proc (symbol-value process-symbol)))
-    ;; If the process is *not* running, start the async command
-    (unless (process-live-p old-proc)
+    ;; If the process is running, kill it and try again
+    (if (process-live-p old-proc)
+        (kill-process old-proc))
       (teleport--tsh-cmd-async
              output-json-symbol
              process-symbol
@@ -278,7 +279,7 @@ COMPLETION-NOTIFICATION when done."
 
 (defun teleport--get-nodes-async-cached
     (&optional completion-notification)
-  "Return cached list of teleport nodes.
+  "Return a list of teleport nodes.
 Call COMPLETION-NOTIFICATION when a new list is available."
   (teleport--tsh-cmd-async-cached 'teleport--nodes-async-cache
                                   'teleport--nodes-async-process

commit 8b33ac666f74d8ab881bdd8d238c36efd518227b

fix: compilation was broken by 15e6148d (kill the running process)

teleport.el
@@ -262,13 +262,12 @@ OUTPUT-JSON-SYMBOL and call COMPLETION-NOTIFICATION when done."
 
   (let ((old-proc (symbol-value process-symbol)))
     ;; If the process is running, kill it and try again
-    (if (process-live-p old-proc)
-        (kill-process old-proc))
-      (teleport--tsh-cmd-async
-             output-json-symbol
-             process-symbol
-             completion-notification
-             cmd)))
+    (when (process-live-p old-proc)
+      (kill-process old-proc)))
+  (teleport--tsh-cmd-async output-json-symbol
+                           process-symbol
+                           completion-notification
+                           cmd)
   (symbol-value output-json-symbol))
 
 (defvar teleport--nodes-async-cache []